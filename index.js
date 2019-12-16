d8762ca155d1842246bc1f3387dcff9f50cfe605
luong <vanluongnbbl@gmail.com>
2019-12-16 18:37:25 +0700

    upload file

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const express = require('express')
 const reload = require('reload')
 
 const multer = require('multer')
-const upload = multer({dest: './public'})
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {cb(null, './public')},
+    filename: (req, file, cb) => cb(null, Date.now() + file.originalname) 
+})
+
+function fileFilter(req, file, cb) {
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpeg'){
+        return cb(null, true)
+    }
+    cb(new Error('File must be PNG or JPG'))
+}
+
+const limits = { fileSize: 102400}
+
+const upload = multer({storage, limits, fileFilter})
 
 const app = express()
 
