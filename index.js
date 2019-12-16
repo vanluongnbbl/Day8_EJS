c7b7ae0a827b6c8fb7021776a50a2a48d8bd0c9f
luong <vanluongnbbl@gmail.com>
2019-12-16 16:03:21 +0700

    ejs

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const reload = require('reload')
+
+const app = express()
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+const arrPeople = [
+    {name: 'Ti', age: 19},
+    {name: 'Teo', age: 20},
+    {name: 'To', age: 21},
+]
+app.locals.arrPeople = arrPeople
+   
+const arrSubjects = ['Mongo', 'Express', 'Angular', 'Nodejs']
+// app.locals.arrSubject = arrSubjects
+
+app.get('/', (req, res) => res.render('home.ejs'))
+app.get('/learn', (req, res) => res.render('learn.ejs', {username: 'Luong Course', arrSubject: arrSubjects}))
+
+reload(app)
+
+app.listen(3000, () => console.log('server running on port 3000'))
+
